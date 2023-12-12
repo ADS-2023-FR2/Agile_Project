e028eed8fd5f815bda0182172a54facf6593578c
sergi <sergicantonsimo@gmail.com>
2023-12-12 16:57:43 +0100

    app and hello problems solved

--- server/app.py
+++ server/app.py
@@ -2,8 +2,10 @@ from flask import Flask, render_template, request, redirect, url_for, session, j
 import os
 import json
 import pandas as pd
+import numpy as np
 
-from src.recommendation_functions import combined_recommendations
+from src.recommendation_functions.combined_recommendations import get_combined_recommendations
+from src.recommendation_functions.recommendations_user import get_ratings
 
 
 app = Flask(__name__)
@@ -21,13 +23,19 @@ top_movies = [
 # Read the CSV file using a relative path
 df = pd.read_csv(os.path.join(os.path.dirname(__file__), 'images/m1movidata.csv'))
 
+dataset = pd.read_csv(os.path.join(os.path.dirname(__file__).replace('server','data'), 'datasets/movielens_1M.csv'))
+nratings = len(dataset)
+
+links = {}
+user_link_list = []
+
 # Check if the JSON file exists, and create it if it doesn't
-if not os.path.exists('users.json'):
-    with open('users.json', 'w') as user_file:
+if not os.path.exists('./users.json'):
+    with open('./users.json', 'w') as user_file:
         json.dump({"users": []}, user_file)
 
 # Load user data from the JSON file
-with open('users.json', 'r') as user_file:
+with open('./users.json', 'r') as user_file:
     user_data = json.load(user_file)
 
 # Define a main route
@@ -60,9 +68,22 @@ def register():
         'password': reg_password
     })
 
+    #Here we create the link
+    while True:
+        random = np.random.randint(nratings)
+        user_id_link = dataset['user_id'][random]
+        if user_id_link not in user_link_list:
+            user_link_list.append(user_id_link)
+            links[reg_username] = user_id_link
+            break
+
     # Save the updated user data to the JSON file
-    with open('server/users.json', 'w') as user_file:
+    with open('./users.json', 'w') as user_file:
         json.dump(user_data, user_file, indent=2)
+    
+    #
+
+    print (links)
 
     return jsonify({'success': True})
 
@@ -80,12 +101,30 @@ def serve_css(filename):
 
 @app.route('/hello', methods=['GET'])
 def hello():
-    return render_template('hello.html', data=df.to_dict(orient='records'))
+    username = session['username']
+    print (username)
+    top_1user = recommendations(links[username])
+    avg_ratings = ratings_dataset(dataset)
+    return render_template('hello.html', data=df, top5=top_1user, avg = avg_ratings)#.to_dict(orient='records'), top5=top_1user)
 
 @app.route('/', methods=['GET', 'POST'])
 def combined_recommendations(user1_id, user2_id, n=5, c=0.5):
     comb_rec = get_combined_recommendations(user1_id, user2_id, n, c)
     return jsonify({'top_recommendations': comb_rec})
+
+@app.route('/', methods=['GET', 'POST'])
+def recommendations(user1_id, top=5):
+    _,rec = get_ratings(user = user1_id)
+    return rec
+    #return jsonify({'top_recommendations': rec})
+
+@app.route('/', methods=['POST'])
+def ratings_dataset(data):
+    avg_ratings = {}
+    for id in np.unique(data['item_id']):
+        aux = dataset[data['item_id']==id]
+        avg_ratings[id] = round (np.mean(aux['rating']),1)
+    return avg_ratings
 
 def load_data_from_csv(csv_file):
     data = {}
